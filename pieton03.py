a328e0cc9a7d64481a9c1f223691fd3f221aa72b
Natchapon <467108@wsk.ac.th>
2026-06-16 02:41:05 +0000

    pieton03.py

---
+++ pieton03.py
@@ -0,0 +1,7 @@
+width = float(input("กรุณาใส่ความกว้าง: ")) # รับค่าความกว้างและความยาวจากผู้ใช้
+length = float(input("กรุณาใส่ความยาว: "))
+
+area = width * length # คำนวณพื้นที่
+
+print("พื้นที่รูปสี่เหลี่ยมมุมฉาก =", area) # แสดงผลลัพธ์
+print("author: Natchapon Pukaew M.6/9")
